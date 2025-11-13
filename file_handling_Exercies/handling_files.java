ffb441721a3e432d85be0865e3ea639f99ca0cc7
codeseeker <secondarycode230651@gmail.com>
2025-11-13 15:33:44 +0500

    Made the populating array from the file part

--- file_handling_Exercies/handling_files.java
+++ file_handling_Exercies/handling_files.java
@@ -4,24 +4,43 @@ public class handling_files{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        try{    
-            FileOutputStream fos = new FileOutputStream("data.txt", true);
-            
+        // putting data into the array
+        int accountsData[][] = { {101,3050,1500},
+                                 {102,2000,2500},
+                                 {103,4500,5500},
+                                 {104,6000,3500} };
+
+        // writing data to the file
+        try {
+            FileOutputStream fos = new FileOutputStream("accounts.txt");
+
             PrintStream ps = new PrintStream(fos);
 
-            while (true) { 
-                System.out.print("Enter text to write to file (type 'exit' to quit): ");
-                String userInput = input.nextLine();
-                
-                if(userInput.equalsIgnoreCase("exit")){
-                    break;
+            for(int i=0; i<accountsData.length; i++){
+                for(int j=0; j<accountsData[i].length; j++){
+                    ps.print(accountsData[i][j]+" ");
                 }
-                
-                ps.println(userInput);
+                ps.println();
             }
-        }
-        catch(Exception e){
+
+            //reading data from the file
+
+            FileInputStream fis = new FileInputStream("accounts.txt");
+            Scanner fileScanner = new Scanner(fis);
+
+            while(fileScanner.hasNext()){
+                int accountNumber = fileScanner.nextInt();
+                int balance = fileScanner.nextInt();
+                int withdrawAmount = fileScanner.nextInt();
+
+                System.out.println("Account Number: " + accountNumber);
+                System.out.println("Balance: " + balance);
+                System.out.println("Withdraw Amount: " + withdrawAmount);
+                System.out.println("---------------------------");
+            }
+        } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
+       
     }
-}
+} 
